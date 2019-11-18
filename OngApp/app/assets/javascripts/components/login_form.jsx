8a5387f48789052d4e56cd455a2b4b027dbd2ef8
El Chino <40839789+FelipeAbadMustillo@users.noreply.github.com>
2019-11-18 17:40:54 -0300

    Added some styles

--- OngApp/app/assets/javascripts/components/login_form.jsx
+++ OngApp/app/assets/javascripts/components/login_form.jsx
@@ -1,15 +1,30 @@
-class LoginForm extends React.Component
+window.LoginForm = createReactClass(
 {
-  render()
+  handleChange: function(e)
+  {
+    user_input={};
+    user_input[e.target.name]=e.target.value;
+    this.props.onUserInput(user_input);
+  },
+
+  handleSubmit: function(e)
+  {
+    e.preventDefault();
+    this.props.onFormSubmit();
+  },
+
+  render: function()
   {
     return(
       <div>
-        <form>
-          <input name='email' placeholder='Dirección de E-Mail'/>
-          <input name='password' placeholder='Contraseña'/>
+        <form onSubmit={this.handleSubmit}>
+          <input name='email' placeholder='Dirección de E-Mail' value={this.props.email_val}
+            onChange={this.handleChange} />
+          <input name='password' placeholder='Contraseña' value={this.props.pass_val}
+            onChange={this.handleChange} />
           <input type='submit' value='Iniciar Sesión'/>
         </form>
       </div>
     )
   }
-}
+});
